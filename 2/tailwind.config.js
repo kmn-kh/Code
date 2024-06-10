e0311076eb6df5c4c5660164e2aac0c32ad9db26
kmn-kh <kmnpro459@gmail.com>
2024-06-10 08:41:59 +0700

    first

---
+++ 2/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Kaushan':"'Kaushan Script', cursive",
+      }
+    },
+  },
+  plugins: [],
+}
